99ef7506160bb47a9b27d45ea9128a0951adbff2
Marco Gnecchi <magnecch@cisco.com>
2017-02-14 23:46:29 +0000

    add circleCI configuration

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-aws ecr get-login --region eu-west-2
+docker_login_cmd='aws ecr get-login --region eu-west-2 --profile HouseMe';
+#echo "$docker_login"
+eval $($docker_login_cmd);
+if [[ $? -ne 0 ]] ; then
+    echo "Docker login failed"
+    exit 1	
+else 
+	echo "Docker login successful"
+fi
 docker build -t houseme .
 docker tag houseme:latest 312827138267.dkr.ecr.eu-west-2.amazonaws.com/houseme:latest
 docker push 312827138267.dkr.ecr.eu-west-2.amazonaws.com/houseme:latest
